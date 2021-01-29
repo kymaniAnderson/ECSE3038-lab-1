e42e84ead2ac65c16c4b4932a7ab286fb4d1d6b6
kymani anderson <kymanianderson@pop-os.localdomain>
2021-01-29 10:00:34 -0500

    completed question 3

--- lab-1.py
+++ lab-1.py
@@ -5,4 +5,26 @@
 def hello():
     print("ECSE3038 - Engineering IoT Systems")
 
+# Question 2: ((INCOMPLETE))
+# 1. pwd must be > 7 characters 
+# 2. pwd must be alphanumeric
+# 3. pwd must have 2 or more numbers
+def validatePassword(pwd):
+    if len(pwd) > 7:      
+        if pwd.isalnum():   
+            return print("hi")
 
+# Question 3:
+# 1. num must be > 1
+# 2. add all values in range 1 - num
+def sumUpToN(num):
+    sum = 0
+    num += 1
+    
+    if num < 1:
+        return -1
+
+    else:
+        for index in range(num):
+            sum += index    
+        return sum
